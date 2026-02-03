ce146dd7576e9dde6af9aec06c86b010bdc3a075
zorboladi428-art <zorboladi428@gmail.com>
2026-02-03 07:25:11 +0300

    Create database.py

---
+++ database.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+conn = sqlite3.connect("users.db", check_same_thread=False)
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    user_id INTEGER PRIMARY KEY,
+    premium_until TEXT
+)
+""")
+conn.commit()
